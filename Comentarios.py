b63590c2be6153c851fe7576347101b54a38f288
jmontantescastillo-dev <jmontantescastillo@gmail.com>
2025-09-23 07:52:57 -0600

    Update and rename Programa 2 to Comentarios.py

---
+++ Comentarios.py
@@ -0,0 +1,12 @@
+Este programa imprime la variable. También observamos la mutabilidad de python
+
+entero = 10
+flotante = 3.14
+ cadena = "Ed Flores"
+booleana = True
+
+print(entero)
+print(flotante)
+
+Ed Flores
+ITPA
